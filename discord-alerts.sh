e427bfed81574f78bbeb6d5087c974abb401f4a0
Mryash7803 <yashsingar5@gmail.com>
2026-05-13 00:24:42 +0530

    added description for discord-alerts.sh

--- discord-alerts.sh
+++ discord-alerts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#Open the file:
+#sudo vi /usr/local/bin/discord-alerts.sh
 
+#Paste this content (replace YOUR_URL_HERE with the URL you copied):
 # Configuration
 WEBHOOK_URL="your discord server url"
 
@@ -18,3 +21,28 @@ tail -Fn0 /var/log/messages /var/log/secure | while read -r line ; do
              "$WEBHOOK_URL"
     fi
 done
+
+---
+#Make it executable:
+
+#sudo chmod +x /usr/local/bin/discord-alerts.sh
+#Create the service file:
+#sudo vi /etc/systemd/system/discord-alerts.service
+
+[Unit]
+Description=Discord Log Monitoring Service
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/discord-alerts.sh
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+
+sudo systemctl daemon-reload
+sudo systemctl enable discord-alerts
+sudo systemctl start discord-alerts
+
+---
